a9f6b5dac88258ba3f74ae30fb6179fa58a2d3e1
LaNMaSteR53 <tjt1980@gmail.com>
2016-11-09 18:29:48 -0500

    Increased the password complexity requirement.

--- server/honeybadger/validators.py
+++ server/honeybadger/validators.py
@@ -1,7 +1,7 @@
 import re
 
-# anything except blank
-PASSWORD_REGEX = r'.+'
+# 1 upper, 1 lower, 1 special, 1 number, minimim 10 chars
+PASSWORD_REGEX = r'(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*\(\)]).{10,}'
 EMAIL_REGEX = r'[^@]+@[a-zA-Z\d-]+(?:\.[a-zA-Z\d-]+)+'
 
 def is_valid_email(email):
