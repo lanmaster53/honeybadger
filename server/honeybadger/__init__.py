c561e2623c18e9fa4adfd3225de677c299c3ede8
LaNMaSteR53 <tjt1980@gmail.com>
2016-11-17 22:31:42 -0500

    Modified the database seed data.

--- server/honeybadger/__init__.py
+++ server/honeybadger/__init__.py
@@ -29,13 +29,13 @@ def initdb(username, password):
     db.session.commit()
     print 'Database initialized.'
     # remove below for production
-    t = models.Target(name='target1', guid='aedc4c63-8d13-4a22-81c5-d52d32293867')
+    t = models.Target(name='demo', guid='aedc4c63-8d13-4a22-81c5-d52d32293867')
     db.session.add(t)
     db.session.commit()
-    b = models.Beacon(target_guid='aedc4c63-8d13-4a22-81c5-d52d32293867', agent='agent1', ip='1.2.3.4', port='80', useragent='Mac OS X', comment='this is a comment.', lat='38.2531419', lng='-85.7564855', acc='5')
+    b = models.Beacon(target_guid='aedc4c63-8d13-4a22-81c5-d52d32293867', agent='HTML', ip='1.2.3.4', port='80', useragent='Mac OS X', comment='this is a comment.', lat='38.2531419', lng='-85.7564855', acc='5')
     db.session.add(b)
     db.session.commit()
-    b = models.Beacon(target_guid='aedc4c63-8d13-4a22-81c5-d52d32293867', agent='agent1', ip='5.6.7.8', port='80', useragent='Mac OS X', comment='this is a comment.', lat='34.855117', lng='-82.114192', acc='1')
+    b = models.Beacon(target_guid='aedc4c63-8d13-4a22-81c5-d52d32293867', agent='HTML', ip='5.6.7.8', port='80', useragent='Mac OS X', comment='this is a comment.', lat='34.855117', lng='-82.114192', acc='1')
     db.session.add(b)
     db.session.commit()
 
