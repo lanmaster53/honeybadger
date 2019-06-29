5eb22a6321446f96c2b9f3ef5facb7ba65da8cf9
Bradley Konsela <bkonsela@localhost.localdomain>
2019-06-28 18:11:51 -0600

    Fix fallback geolocation, add external ip address enumeration via ipify.org

--- server/honeybadger/plugins.py
+++ server/honeybadger/plugins.py
@@ -1,5 +1,6 @@
 from honeybadger import app, logger
-import urllib2
+import urllib
+from urllib.request import urlopen
 import json
 
 def get_coords_from_google(aps):
@@ -10,8 +11,9 @@ def get_coords_from_google(aps):
         data['wifiAccessPoints'].append(ap.serialized_for_google)
     data_json = json.dumps(data)
     headers = {'Content-Type': 'application/json'}
-    request = urllib2.Request(url, data=data_json, headers=headers)
-    response = urllib2.urlopen(request)
+    request = urllib.request.Request(url, data="{}".encode(), headers=headers)
+    print(data_json)
+    response = urlopen(request)
     content = response.read()
     logger.info("Google API response: {}".format(content))
     jsondata = None
@@ -26,11 +28,17 @@ def get_coords_from_google(aps):
         data['lng'] = jsondata['location']['lng']
     return data
 
-def get_coords_from_uniapple(ip):
-    logger.info('Geolocating via Uniapple API.')
-    url = 'http://uniapple.net/geoip/?ip={}'.format(ip)
-    content = urllib2.urlopen(url).read()
-    logger.info('Uniapple API response:\n{}'.format(content))
+def get_external_ip(ip):
+    logger.info('Resolving external IP via Ipify API.')
+    url = "https://api.ipify.org"
+    content = urlopen(url).read()
+    return content.decode()
+
+def get_coords_from_ipstack(ip):
+    logger.info('Geolocating via Ipstack API.')
+    url = 'http://api.ipstack.com/{0}?access_key={1}'.format(get_external_ip(ip), app.config['IPSTACK_API_KEY'])
+    content = urlopen(url).read()
+    logger.info('Ipstack API response:\n{}'.format(content))
     jsondata = None
     try:
         jsondata = json.loads(content)
