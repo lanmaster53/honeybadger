ed3a3f46137d054d31a058c7e3242c5b9106ee8e
Brad Konsela <bradley@blackhillsinfosec.com>
2019-07-02 11:25:58 -0600

    Added comments to wireless_survey.sh

--- util/wireless_survey.sh
+++ util/wireless_survey.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Get wireless information
 wireless_data=$(iwlist scan 2>&1 | egrep 'Address|ESSID|Signal')
+
+# Create post data string
 post_data="os=linux&data=$(echo "$wireless_data" | base64 -w 0)"
 
+# Send the data to the URL supplied via command line argument
 curl -d "$post_data" "$1/CMD"
