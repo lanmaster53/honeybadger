76eebcf9f7fd1b9098bb3e09680d5ed2c908a57c
Brad Konsela <bradley@blackhillsinfosec.com>
2019-07-03 14:59:41 -0600

    Added comments to powershell script

--- util/wireless_survey.ps1
+++ util/wireless_survey.ps1
@@ -1,10 +1,19 @@
+# Accept a command-line argument for the URI
 param (
     [string]$uri = ""
 )
 
+# Poll for wireless network information with Netsh
 $wifi = netsh wlan show networks mode=bssid | findstr "SSID Signal Channel"
+
+# Set encoding of wifi data
 $wifibytes = [System.Text.Encoding]::UTF8.GetBytes($wifi)
+
+# Base64-encode the wifi data bytes
 $wifienc = [Convert]::ToBase64String($wifibytes)
 
+# Assemble post data
 $postdat = @{os='windows';data=$wifienc}
+
+# Send POST request to server, using CMD as agent
 Invoke-WebRequest -Uri "$uri/CMD" -Method POST -Body $postdat
