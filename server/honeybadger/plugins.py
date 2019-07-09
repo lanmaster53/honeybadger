177d2a3f79d6af4897394053dcfe32231caf5530
lanmaster53 <tjt1980@gmail.com>
2019-07-08 22:21:50 -0700

    Renamed `request` to `response` to more accurately reflect what the object is.

--- server/honeybadger/plugins.py
+++ server/honeybadger/plugins.py
@@ -10,11 +10,11 @@ def get_coords_from_google(aps):
         data['wifiAccessPoints'].append(ap.serialized_for_google)
     data_json = json.dumps(data)
     headers = {'Content-Type': 'application/json'}
-    request = requests.post(url=url, data=data_json, headers=headers)
-    logger.info("Google API response: {}".format(request.content))
+    response = requests.post(url=url, data=data_json, headers=headers)
+    logger.info("Google API response: {}".format(response.content))
     jsondata = None
     try:
-        jsondata = request.json()
+        jsondata = response.json()
     except ValueError as e:
         logger.error('{}.'.format(e))
     data = {'lat':None, 'lng':None, 'acc':None}
@@ -27,11 +27,11 @@ def get_coords_from_google(aps):
 def get_coords_from_ipstack(ip):
     logger.info('Geolocating via Ipstack API.')
     url = 'http://api.ipstack.com/{0}?access_key={1}'.format(ip, app.config['IPSTACK_API_KEY'])
-    request = requests.get(url)
-    logger.info('Ipstack API response:\n{}'.format(request.content))
+    response = requests.get(url)
+    logger.info('Ipstack API response:\n{}'.format(response.content))
     jsondata = None
     try:
-        jsondata = request.json()
+        jsondata = response.json()
     except ValueError as e:
         logger.error('{}.'.format(e))
 
@@ -53,11 +53,11 @@ def get_coords_from_ipinfo(ip):
     # New fallback, ipinfo doesn't require an API key for a certain number of API calls
     logger.info('Geolocating via Ipinfo.io API.')
     url = 'https://ipinfo.io/{}'.format(ip)
-    request = requests.get(url)
-    logger.info('Ipinfo.io API response:\n{}'.format(request.content))
+    response = requests.get(url)
+    logger.info('Ipinfo.io API response:\n{}'.format(response.content))
     jsondata = None
     try:
-        jsondata = request.json()
+        jsondata = response.json()
     except ValueError as e:
         logger.error('{}.'.format(e))
     data = {'lat':None, 'lng':None}
